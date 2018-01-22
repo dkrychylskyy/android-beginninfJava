4d25dc5c11699c36931992d133f8feb62ecccd23
Dimitry <d.krychylskyy@gmail.com>
2018-01-22 21:59:51 +0100

    On a commencé avec couldthisbelove

--- couldthisbelove/src/main/java/com/example/couldthisbelove/LoveCalc.java
+++ couldthisbelove/src/main/java/com/example/couldthisbelove/LoveCalc.java
@@ -1,4 +1,9 @@
 package com.example.couldthisbelove;
 
 public class LoveCalc {
+    public static void main(String[] args) {
+        System.out.println("Your love score is ");
+    }
+
+    public static int
 }
--- javapractice/src/main/java/com/example/javapractice/MyClass.java
+++ javapractice/src/main/java/com/example/javapractice/MyClass.java
@@ -3,6 +3,31 @@ package com.example.javapractice;
 public class MyClass {
 
     public static void main(String[] args) {
+        int change = getMilk(5, 40 );
+        System.out.println("Hello Master, your change is " + change + " pounds");
+    }
 
+    /*public static void getMilk(int nrOfCartonsToBuy) {
+
+        int priceToPay = nrOfCartonsToBuy*2;
+
+        System.out.println("Open door");
+        System.out.println("Walk to store");
+        System.out.println("By "+ nrOfCartonsToBuy + " milk on the ground floor");
+        System.out.println("Pay " + priceToPay + " pounds, but no more");
+        System.out.println("Return home with milk galore");
+    }
+    */
+    public static int getMilk(int nrOfCartonsToBuy, int startingAmount) {
+
+        int priceToPay = nrOfCartonsToBuy*2;
+
+        System.out.println("Open door");
+        System.out.println("Walk to store");
+        System.out.println("By "+ nrOfCartonsToBuy + " milk on the ground floor");
+        System.out.println("Pay " + priceToPay + " pounds, but no more");
+        System.out.println("Return home with milk galore");
+
+        return startingAmount - priceToPay;
     }
 }
