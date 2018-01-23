1c910dbfa9df69e325bbaf6d2f7c80bb6655ecd6
Dimitry <d.krychylskyy@gmail.com>
2018-01-23 19:44:07 +0100

    If Else conditions

--- couldthisbelove/src/main/java/com/example/couldthisbelove/LoveCalc.java
+++ couldthisbelove/src/main/java/com/example/couldthisbelove/LoveCalc.java
@@ -1,9 +1,26 @@
 package com.example.couldthisbelove;
 
+import java.util.Random;
+
 public class LoveCalc {
     public static void main(String[] args) {
-        System.out.println("Your love score is ");
+        int loveScore = ifYouHadMyLove("Mr X.", "Miss Y.");
+        System.out.println("Your love score is " + loveScore );
     }
 
-    public static int
+    public static int ifYouHadMyLove(String yourName, String theirName){
+
+        Random randomObj = new Random();
+        int loveScore = randomObj.nextInt(101);
+
+        if (loveScore > 80){
+            System.out.println("You love each other like Kanye loves Kanye");
+        } else if (loveScore > 40){
+            System.out.println("You go together like coke and mentos");
+        } else {
+            System.out.println("No love possible. You'il be forever alone...");
+        }
+
+        return loveScore;
+    }
 }
